62c6a54819236f5363076ca7dfc11289c32e0869
Éric  António <ericantoniodev@gmail.com>
2023-08-03 20:46:50 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Dashboard } from './components/Dashboad';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global'
 export function App() {
   return (
     <>
       <Header/>
+      <Dashboard/>
       <GlobalStyle/>
     </>
   );
