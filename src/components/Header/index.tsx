c1f95f6b878b120c8f964d9281eed112aeeb9058
Éric  António <ericantoniodev@gmail.com>
2023-08-06 20:51:56 +0200

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,6 +7,12 @@ import { Container , Content } from './styles';
 export function Header(){
     return(
         <Container>
+            <Content>
+                <img src={logoImg} alt='Dt money'/>
+                <button type='button'>
+                Nova Transação 
+                </button>
+            </Content>
 
         </Container>
     )
