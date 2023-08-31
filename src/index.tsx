d0689c4797d2d74530530fbc83fef02623a6201f
Éric  António <ericantoniodev@gmail.com>
2023-08-31 16:40:21 +0200

    npm install --save-dev miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import{ createServer } from 'miragejs'
 import {App} from './App';
 
+createServer({
+  routes(){
+    this.namespace = 'api'
+  }
+})
+
+
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+  document.getElementById('root') as HTMLElement);root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
