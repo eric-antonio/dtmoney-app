da664a78c7d71e43796b48cc038eeb6b0dc2da09
Éric  António <ericantoniodev@gmail.com>
2023-09-06 15:15:05 +0200

    Modal is openig

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Modal from 'react-modal';
 
 import logoImg from '../../assets/Logo.svg'
 import { Container , Content } from './styles';
 
 
 export function Header(){
+
+    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+    function handelNewOpenTransactionModal(){
+        setIsNewTransactionModalOpen(true);
+
+    }
+
+    function handelCloseTransactionModal(){
+        setIsNewTransactionModalOpen(false);
+    }
     return(
         <Container>
             <Content>
                 <img src={logoImg} alt='Dt money'/>
-                <button type='button'>
-                Nova Transação 
+                <button type='button' onClick={handelNewOpenTransactionModal}>
+                    Nova Transação 
                 </button>
+
+                <Modal isOpen={isNewTransactionModalOpen}>
+                    <h2>Cadastrar Transação</h2>
+                </Modal>
             </Content>
 
         </Container>
