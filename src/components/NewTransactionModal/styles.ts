d61732cb91a7e5919caeb9cc9d1efc48f3215fc9
Éric  António <ericantoniodev@gmail.com>
2023-09-06 19:02:38 +0200

    Update styles.ts

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -51,5 +51,31 @@ export const Container = styled.div`
 `;
 
 export const TransactionTypeContainer =styled.div`
+  margin: 1rem 0 ;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 0.5rem;
 
+  button{
+    height: 4rem;
+    border: 1px solid;
+    border-radius:0.25rem ;
+
+    background:  transparent;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    img{
+      width: 20px;
+      height: 20px;
+    }
+    span{
+      display: inline-block;
+      margin-left: 1rem;
+      font-size: 1rem;
+      color: var(--text);
+    }
+  }
 `;
